8fbcef4ca8b7591b3a700dcb5f6db5c066adbd80
Maor <maor>
2018-12-28 20:03:43 +0200

    if and while works

--- src/js/code-cfg_builder.js
+++ src/js/code-cfg_builder.js
@@ -1,5 +1,5 @@
 import {parseCode} from './code-analyzer';
-import {convertFlowchartToGraph} from './statement-parser';
+import {convertJsonChartToGraph} from './statement-parser';
 import * as esgraph from 'esgraph';
 import * as js2flowchart from 'js2flowchart';
 import * as Styx from 'styx';
@@ -7,20 +7,10 @@ import * as Styx from 'styx';
 
 export function extractGraphFromCode(code, inputVector) {
     let parsedCode = parseCode(code);
-    // let graph = new Graph();
-    // graphGenerator(esgraph(parsedCode)[0], graph, inputVector);
-    // let lastNode = graph.getLastNode();
-    // let currentNode = graph.addNode('end node', 'end');
-    // graph.addEdge(lastNode, currentNode);
 
-    var flowProgram = Styx.parse(parsedCode);
-    var json = Styx.exportAsJson(flowProgram);
-
-    // graphGenerator(json, graph, inputVector);
-
-
-
-    return convertFlowchartToGraph(json);
+    let flowProgram = Styx.parse(parsedCode);
+    let json = Styx.exportAsJson(flowProgram);
+    return convertJsonChartToGraph(json);
 }
 
 export const buildGraph = (code, inputVector) => {
--- src/js/statement-parser.js
+++ src/js/statement-parser.js
@@ -1,105 +1,5 @@
 import {evalCode} from './code-analyzer';
 
-let parseFunctions = {
-    BlockStatement: parseBlockStatement,
-    Program: parseBlockStatement,
-    VariableDeclaration: parseVariableDeclaration,
-    ExpressionStatement: parseExpressionStatement,
-    FunctionDeclaration: parseFunctionDeclaration,
-    WhileStatement: parseWhileStatement,
-    IfStatement: parseIfStatement,
-    ReturnStatement: parseReturnStatement,
-};
-
-function addNewsEdges(previousNodes, currentNode, graph) {
-    if (previousNodes) {
-        // console.log('prev nodes = ' + JSON.stringify(previousNodes));
-        for (let i = 0; i < previousNodes.length; i++) {
-            let previousNode = previousNodes[i];
-            let condition = '';
-            if (previousNode.type === 'condition true')
-                condition = 'true';
-            else if (previousNode.type === 'condition false')
-                condition = 'false';
-            graph.addEdge(previousNode.name, currentNode, condition);
-        }
-    }
-}
-
-function parseVariableDeclaration(parsedCode, graph, previousNodes) {
-    let currentNode = graph.addNode(evalCode(parsedCode));
-    addNewsEdges(previousNodes, currentNode, graph);
-    return [{name: currentNode, type: 'assignment'}];
-}
-
-function parseExpressionStatement(parsedCode, graph, previousNode) {
-    return null;
-}
-
-function parseFunctionDeclaration(parsedCode, graph, previousNodes) {
-    console.log('parseFunctionDeclaration');
-    return graphGenerator(parsedCode.body, graph, previousNodes);
-}
-
-function parseBlockStatement(parsedEsgraphNode, graph, previousNodes) {
-    // graph.resetBlock();
-    // return parseStatementList(parsedEsgraphNode.body, graph, previousNodes);
-    graphGenerator(parsedEsgraphNode.normal, graph, previousNodes);
-    return null;
-}
-
-function parseStatementList(statementList, graph, previousNodes) {
-    let lastNode = previousNodes;
-    for (let i = 0; i < statementList.length; i++) {
-        lastNode = graphGenerator(statementList[i], graph, lastNode);
-    }
-    return lastNode;
-}
-
-function parseWhileStatement(parsedCode, graph, inputVector) {
-    return null;
-}
-
-function parseIfStatement(parsedCode, graph, previousNodes) {
-    let nodeName = graph.addNode(evalCode(parsedCode.test), 'condition');
-
-    let endTrueNode = nodeName;
-    let endFalseNode = nodeName;
-    addNewsEdges(previousNodes, nodeName, graph);
-
-    let consequentNode = graphGenerator(parsedCode.consequent, graph, [{name: nodeName, type: 'condition true'}]);
-    if (consequentNode) {
-        endTrueNode = consequentNode[0].name;
-    }
-    if (parsedCode.alternate) {
-        let alternateNode = graphGenerator(parsedCode.alternate, graph, [{name: nodeName, type: 'condition false'}]);
-        if (alternateNode) {
-            endTrueNode = alternateNode[0].name;
-        }
-    }
-
-    // graph.addEdge(nodeName, graph.getLastNode() + 1);
-    // parsedCode.alternate = substituteStatement(parsedCode.alternate);
-    if (endTrueNode === endFalseNode)
-        return [{name: endTrueNode, type: 'end condition'}];
-    else
-        return [{name: endTrueNode, type: 'end condition'}, {name: endFalseNode, type: 'end condition'}];
-}
-
-function parseReturnStatement(parsedCode, graph, previousNodes) {
-    let currentNode = graph.addNode(evalCode(parsedCode), 'return');
-    addNewsEdges(previousNodes, currentNode, graph);
-    return [{name: currentNode, type: 'return'}];
-}
-
-export function graphGenerator(parsedEsgraphNode, graph, previousNodes = null) {
-    console.log(parsedEsgraphNode.astNode);
-    if (parsedEsgraphNode.astNode !== undefined && parsedEsgraphNode.astNode.type in parseFunctions) {
-        return parseFunctions[parsedEsgraphNode.astNode.type](parsedEsgraphNode, graph, previousNodes);
-    }
-    else
-        return null;
-}
 
 function addNodeToGraph(node, graph) {
     if (!(node.id in graph.getNodeMap())) {
@@ -112,15 +12,7 @@ function addNodeToGraph(node, graph) {
     }
 }
 
-export function convertFlowchartToGraph(flowchart) {
-    flowchart = JSON.parse(flowchart);
-    let functionJson = flowchart.functions[0];
-    // console.log(functionJson);
-    let graph = new Graph();
-    if (functionJson === undefined)
-        return graph;
-    let flowGraph = functionJson.flowGraph;
-    // console.log(flowGraph);
+function createNodeMap(flowGraph) {
     let nodeMap = {};
 
     for (let i = 0; i < flowGraph.nodes.length; i++) {
@@ -128,21 +20,27 @@ export function convertFlowchartToGraph(flowchart) {
         nodeMap[node.id] = node;
 
     }
+    return nodeMap;
+}
 
-    // let edges = [];
+function generateGraph(flowGraph, graph, nodeMap) {
     for (let i = 0; i < flowGraph.edges.length; i++) {
         let edge = flowGraph.edges[i];
         edgeParser(edge, graph, nodeMap);
     }
     let lastEdge = graph.getLastEdge();
-    console.log('last edge ' + lastEdge);
     addNodeToGraph(nodeMap[lastEdge.to], graph);
-    // let nodesIds = Object.keys(nodeMap);
-    // for (let i = 0; i < nodesIds.length; i++) {
-    //     let node = nodeMap[nodesIds[i]];
-    //     addNodeToGraph(node, graph);
-    // }
-    // graph.validateEdges(nodeMap);
+}
+
+export function convertJsonChartToGraph(flowchart) {
+    flowchart = JSON.parse(flowchart);
+    let functionJson = flowchart.functions[0];
+    let graph = new Graph();
+    if (functionJson === undefined)
+        return graph;
+    let flowGraph = functionJson.flowGraph;
+    let nodeMap = createNodeMap(flowGraph);
+    generateGraph(flowGraph, graph, nodeMap);
     return graph;
 }
 
@@ -225,27 +123,13 @@ class Graph {
     }
 
     addNode(name, data, type = '') {
-        // let name = this.nodes.length + 1;
         let lastNode = this.nodes[this.nodes.length - 1];
         if (lastNode !== undefined && type === 'assignment' && lastNode.type === type && lastNode.name + 1 === name) {
             lastNode.data.push(data);
             let oldName = lastNode.name;
             lastNode.name = name;
 
-            let newEdges = [];
-            for (let i = 0; i < this.edges.length; i++) {
-                let edge = this.edges[i];
-                if (edge.to === oldName) {
-                    edge.to = name;
-                    newEdges.push(edge);
-                } else if (edge.from === oldName) {
-                    edge.from = name;
-                    // newEdges.push(edge);
-                }
-                else
-                    newEdges.push(edge);
-            }
-            this.edges = newEdges;
+            this._fixEdges(oldName, name);
         }
         else {
             this.nodes.push({name: name, data: [data], type: type});
@@ -253,43 +137,20 @@ class Graph {
         return name;
     }
 
-
-    validateEdges() {
-        let nodeMap = this.getNodeMap();
-        console.log(nodeMap);
-        console.log(this.edges);
+    _fixEdges(oldName, name) {
         let newEdges = [];
-
         for (let i = 0; i < this.edges.length; i++) {
             let edge = this.edges[i];
-            if (!(edge.to in nodeMap)) {
-                console.log('not found: ' + edge.to);
-                i = this.findNextExistsNode(i, nodeMap, newEdges);
+            if (edge.to === oldName) {
+                edge.to = name;
+                newEdges.push(edge);
+            } else if (edge.from === oldName) {
+                edge.from = name;
             }
             else
                 newEdges.push(edge);
         }
         this.edges = newEdges;
-    }
-
-    findNextExistsNode(startIndex, nodeMap, newEdges) {
-        let edgeMap = {};
-        for (let i = 0; i < this.edges.length; i++) {
-            let edge = this.edges[i];
-            edgeMap[edge.from] = edge.to;
-        }
-        let edge = this.edges[startIndex];
-        for (let j = startIndex + 1; j < this.edges.length; j++) {
-            let nextEdge = this.edges[j];
-            if (nextEdge.to in nodeMap) {
-                console.log(nextEdge.to);
-                edge.to = nextEdge.to;
-                newEdges.push(edge);
-                startIndex = j;
-                break;
-            }
-        }
-        return startIndex;
     }
 
     getNodeMap() {
